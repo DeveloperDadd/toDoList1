897e6526977bdf4e51c261bd7664811c5d344b9f
claytoncritchfield <clayton.critchfield@gmail.com>
2023-07-06 16:57:28 -0400

    got the buttons to work properly to filter the page correctly

--- src/routes/Tasklist.js
+++ src/routes/Tasklist.js
@@ -1,2 +1,11 @@
-import React from "react";
+import React from 'react';
 
+export default function Tasklist ({ filteredTasks }) {
+    return(
+        <div id="test">
+         <ul>
+         {filteredTasks.map(task => {return (<li key={task.id}><input type="checkbox"></input><p>{task.text}</p></li>)})}
+         </ul>
+        </div>
+    )
+};
