c07440eb8b28b07f0f8968ce10c585cf543e3691
claytoncritchfield <clayton.critchfield@gmail.com>
2023-07-06 10:38:33 -0400

    started the to do list footer, import Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import Heading from './routes/Heading';
 import Container from './routes/Container';
+import Footer from './routes/Footer';
 
 function App() {
+
+  const buttons = {
+
+  }
+
   return (
     <>
     <Heading title="TODOS"/>
-    <Container>
-
-      
-    </Container>
+    <div className='container-sm border border-dark'>
+      <Footer />
+    </div>
     </>
   );
 }
---
+++ src/routes/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Footer() {
+    return(
+        <>
+            <p>Count</p>
+            <div>
+            <button className='btn'>All</button>
+            <button className='btn'>Active</button>
+            <button className='btn'>Completed</button>
+            </div>
+            <button></button>
+        </>
+    )
+}
