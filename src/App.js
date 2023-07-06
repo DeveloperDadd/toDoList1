d268a5593ae688931eea47bbc27fe153e13af486
claytoncritchfield <clayton.critchfield@gmail.com>
2023-07-06 14:35:33 -0400

    got enter key to work on the input field, so it collects user input, puts into the tasks array, and then clear the input field

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import Heading from './routes/Heading';
-import Container from './routes/Container';
+import Task from './routes/Task';
 import Footer from './routes/Footer';
 import { useState } from 'react';
 
 function App() {
-  let tasks = [];
-  let count = tasks.length;
-  const [display, setDisplay] = useState([tasks]);
+  
+  const [tasks, setTasks] = useState([]);
+  const [tempData, setTempData] = useState("");
 
-  const onAll = () => {
-    
-  };
+  const handleKeyDown = (e) => {
+    setTempData(e.target.value);
+    if (e.key ==='Enter') {
+      const task = {
+        text : tempData,
+        id : Date.now(),
+        active : true
+      }
+      let tempTasks = [...tasks, task];
+      setTasks(tempTasks);
+      setTempData("");
+    }
+}
 
-  const footer = [count, ['All', 'Active', 'Completed','Clear Completed']];
+  const taskPlaceholder = {
+    text : 'Enter a task to be completed'
+  }
+  
+  const footer = [tasks.length, ['All', 'Active', 'Completed','Clear Completed']];
 
   return (
     <>
     <Heading title="TODOS"/>
     <div className='container-sm border border-dark'>
+      <Task placeholder={tempData} handleKeyDown={handleKeyDown} />
       <Footer count={`${tasks.length} items left`} all={footer[1][0]} active={footer[1][1]} completed={footer[1][2]} clearCompleted={footer[1][3]}/>
     </div>
     </>
