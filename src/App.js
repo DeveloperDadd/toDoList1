65a64f4bca99b7be35fb7dbf77afb88ab430ab68
claytoncritchfield <clayton.critchfield@gmail.com>
2023-07-06 10:23:39 -0400

    installed bootstrap via npm, created Container component - need to style it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import Heading from './routes/Heading';
+import Container from './routes/Container';
 
 function App() {
   return (
     <>
     <Heading title="TODOS"/>
+    <Container>
+
+      
+    </Container>
     </>
   );
 }
---
+++ src/routes/Container.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default function Container() {
+
+    return <div className='container-fluid'></div>
+}
