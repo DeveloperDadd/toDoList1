110eac6b768d77eadac2f056671fc32f633166cb
claytoncritchfield <clayton.critchfield@gmail.com>
2023-07-06 10:26:54 -0400

    initial CSS styling to get elements centered

--- src/routes/Container.js
+++ src/routes/Container.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
 export default function Container() {
-
-    return <div className='container-fluid'></div>
+    return <div className='container-sm border border-dark'></div>
 }
