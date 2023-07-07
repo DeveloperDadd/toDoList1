eee3a43e58cb0ca9891268e355d7d4856f7afe84
claytoncritchfield <clayton.critchfield@gmail.com>
2023-07-07 08:50:04 -0400

    got the deleted buttons to work

--- src/routes/Tasklist.js
+++ src/routes/Tasklist.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-export default function Tasklist ({ filteredTasks }) {
+export default function Tasklist ({ filteredTasks, deleteTask }) {
     return(
         <div id="test">
          <ul className='list-group'>
-         {filteredTasks.map(task => {return (<li key={task.id} className='list-group-item d-flex justify-content-between align-items-center'><input type="checkbox" className='form-check-input'></input><p className='my-1'>{task.text}</p><button className='btn btn-danger'>X</button></li>)})}
+         {filteredTasks.map(task => {return (<li key={task.id} className='list-group-item d-flex justify-content-between align-items-center'><input type="checkbox" className='form-check-input'></input><p className='my-1'>{task.text}</p><button className='btn btn-danger' onClick={() => deleteTask(task.id)}>X</button></li>)})}
          </ul>
         </div>
     )
