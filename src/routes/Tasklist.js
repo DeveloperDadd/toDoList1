e24257367b9d5aef43e2798e0b7ff0c1574dde6e
claytoncritchfield <clayton.critchfield@gmail.com>
2023-07-07 08:07:01 -0400

    added more CSS styling to the list items

--- src/routes/Tasklist.js
+++ src/routes/Tasklist.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default function Tasklist ({ filteredTasks }) {
     return(
         <div id="test">
-         <ul>
-         {filteredTasks.map(task => {return (<li key={task.id}><input type="checkbox"></input><p>{task.text}</p></li>)})}
+         <ul className='list-group'>
+         {filteredTasks.map(task => {return (<li key={task.id} className='list-group-item d-flex justify-content-between align-items-center'><input type="checkbox" className='form-check-input'></input><p className='my-1'>{task.text}</p><button className='btn btn-danger'>X</button></li>)})}
          </ul>
         </div>
     )
