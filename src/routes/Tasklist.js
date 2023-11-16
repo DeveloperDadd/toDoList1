06ddef981192b430d783c90fd4fbc00e279691d4
claytoncritchfield <clayton.critchfield@gmail.com>
2023-11-15 23:45:12 -0500

    Add double click functionality on todo items so user can edit their entry if needed

--- src/routes/Tasklist.js
+++ src/routes/Tasklist.js
@@ -1,12 +1,66 @@
 import React from 'react';
+import { useState } from 'react';
 
-export default function Tasklist ({ filteredTasks, deleteTask, markComplete }) {
+export default function Tasklist ({ filteredTasks, deleteTask, markComplete, handleKeyDown }) {
+    const [editingTaskId, setEditingTaskId] = useState(null);
+    const [text, setText] = useState("");
+
+    const handleDoubleClick = (taskId) => {
+        setEditingTaskId(taskId);
+        setText(filteredTasks.find(task=>task.id === taskId).text)
+    };
+
+    const handleChange = (event) => {
+        setText(event.target.value);
+    }
+
+    const handleBlur = () => {
+        updateTask();
+    };
+
+    const handleKeyPress = (event) => {
+        if (event.key === 'Enter') {
+            updateTask();
+        }
+    }
+
+    const updateTask = () => {
+        const taskIndex = filteredTasks.findIndex(task => task.id === editingTaskId);
+        if (taskIndex !== -1) {
+            const updatedTasks = [...filteredTasks];
+            updatedTasks[taskIndex].text=text;
+        }
+
+        setEditingTaskId(null);
+        setText("");
+    }
+
+
+
+
     return(
         <div id="test">
          <ul className='list-group'>
          {filteredTasks.map(task => {return (<li key={task.id} className='list-group-item d-flex justify-content-between align-items-center'>
             <input type="checkbox" checked={!task.isActive} isActive={task.isActive} onChange={e => markComplete(task.id, e.target.check)} className='form-check-input'></input>
-            <p className='my-1'>{task.text}</p>
+            
+            <div onDoubleClick={() => handleDoubleClick(task.id)}>
+                {editingTaskId === task.id ? (
+                    <input
+                        type='text'
+                        value={text}
+                        onChange={handleChange}
+                        onKeyDown={handleKeyPress}
+                        onBlur={handleBlur}
+                    />
+                ) : (
+                    <p className='my-1'>{task.text}</p>
+                )}
+            </div>
+            
+
+            
+            
             <button className='btn btn-danger' onClick={() => deleteTask(task.id)}>X</button></li>)})}
          </ul>
         </div>
